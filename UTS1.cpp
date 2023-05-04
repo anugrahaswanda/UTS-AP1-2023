d9332931de2c1c362b68154134b464ffc1e6ddfe
Nuga <114504650+anugrahaswanda@users.noreply.github.com>
2023-05-04 12:59:15 +0700

    Add files via upload

---
+++ UTS1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Masukkan nilai n: ";
+    cin >> n;
+
+    for (int i = n-1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        for (int j = 3; j <= 2*(n-i); j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n-1; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        for (int j = 3; j <= 2*(n-i); j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
